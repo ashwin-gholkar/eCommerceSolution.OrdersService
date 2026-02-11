c6be0085e3c6e7329f9f2ce88a2d921d8e03b0d9
ashwin <ashwingholkar@outlook.com>
2026-02-11 17:13:28 +0530

    Add MongoDB support and DI setup in DataAccessLayer
    Added MongoDB.Driver package and configured MongoDB connection using environment variables in appsettings.json. Updated DependencyInjection to register IMongoClient and IMongoDatabase for dependency injection.

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -8,7 +8,17 @@ namespace DataAccessLayer
         public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
         {
 
+            string conString = configuration.GetConnectionString("MongoDb")!;
+            conString.Replace("$MONGO_HOST",
+                Environment.GetEnvironmentVariable("MONGODB_HOST"))
+                .Replace("MONGODB_PORT",
+                Environment.GetEnvironmentVariable("MONGODB_PORT"));
             // Add other repositories as needed
+
+
+            services.AddSingleton<IMongoClient>(new MongoClient(conString));
+
+            services.AddScoped<IMongoDatabase>(provider => { });
             return services;
         }
     }
